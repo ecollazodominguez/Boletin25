9cbbc4f94c83f9e3f8c6f153e2445b8939d982d6
ecollazodominguez <ecollazodominguez@nautilus9.danielcastelao.org>
2019-04-01 11:47:54 +0200

    v1

---
+++ src/boletin25/Boletin25.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin25;
+
+/**
+ *
+ * @author ecollazodominguez
+ */
+public class Boletin25 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       Ventana ven = new Ventana();
+       ven.setVisible(true);
+       ven.setDefaultCloseOperation(3);
+       Ventana2 ven2 = new Ventana2();
+       ven2.setVisible(true);
+       ven2.setDefaultCloseOperation(3);
+    }
+    
+}
